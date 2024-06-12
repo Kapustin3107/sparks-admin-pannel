382856ce0b5a586da3e7075df4c916eb9d90c424
Yurii Kapustin <yuriy.zieit@gmail.com>
2024-06-12 08:23:30 +0200

    credentials change

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       connection: {
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME', 'db'),
+        user: env('DATABASE_USERNAME', 'db'),
+        password: env('DATABASE_PASSWORD', 'db'),
         schema: env('DATABASE_SCHEMA', 'public'), // Not required
         ssl: {
           ca: env('DATABASE_CA')
